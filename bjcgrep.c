7243192f8d28b40bac81d9936391ae43a081c9ed
Benjamin Cooke <bcooke@freedomofknowledge.org>
2018-09-17 21:31:25 -0400

    Add license

--- bjcgrep.c
+++ bjcgrep.c
@@ -1,3 +1,10 @@
+/*
+* Copyright © 2018 Benjamin Cooke <bcooke@freedomofknowledge.org>
+* This work is free. You can redistribute it and/or modify it under the
+* terms of the Do What The Fuck You Want To Public License, Version 2,
+* as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
