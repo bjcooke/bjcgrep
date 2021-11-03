9487702ecd1556551773d998502a34f64ad3e94d
Benjamin Cooke <bcooke1@umbc.edu>
2021-11-03 17:56:11 -0400

    Update email address

--- bjcgrep.c
+++ bjcgrep.c
@@ -1,5 +1,5 @@
 /*
-* Copyright © 2018 Benjamin Cooke <bcooke@freedomofknowledge.org>
+* Copyright © 2018 Benjamin Cooke <bjcooke92@gmail.com>
 * This work is free. You can redistribute it and/or modify it under the
 * terms of the Do What The Fuck You Want To Public License, Version 2,
 * as published by Sam Hocevar. See the COPYING file for more details.
